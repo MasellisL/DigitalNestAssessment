24276ae6609681cbce3b1c1d3665d3f1810981f8
Leo Masellis <leomasellis@gmail.com>
2025-05-27 12:11:41 -0700

    adjusting router for gh pages

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -6,7 +6,7 @@ import NavBar from '../../components/nav-bar/nav-bar-component';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/DigitalNestAssessment/">
       <NavBar />
       <main className="main">
         <Routes>
